8a42738cbbf606424aa12a2fbc91067d92b65b8e
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-12-18 23:26:13 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import logo from "../src/assets/mars-logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full flex flex-col text-black overflow-hidden">
+        <nav className="absolute top-0 left-0 w-full z-40">
+          <div className="max-w-[1440px] mx-auto px-4 md:px-8 lg:px-12 w-full">
+            <div className="flex items-center justify-between py-2">
+              <div className=" items-center">
+                <img src={logo} className="h-8" alt="logo" />
+              </div>
+
+              <div className="flex gap-2 text-white">
+                <a href="">À Propos</a>
+                <a href="">Nos Marques</a>
+                <a href="">Développement Durable</a>
+                <a href="">Actus et témoignages</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className="relative w-full h-full ">
+          <video
+            src="/video_header.mp4"
+            playsInline
+            className="w-full
+            h-screen
+          object-cover
+          brightness-[75%]"
+            autoPlay
+            muted
+            loop
+          ></video>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
