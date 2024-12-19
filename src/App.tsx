c6e9b8ccd81ca654e8291698098678282e483da7
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-12-19 22:24:37 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
 import "./App.css";
 import logo from "../src/assets/mars-logo.png";
+import { useState, useEffect } from "react";
 
 import { FiSearch } from "react-icons/fi";
 
 function App() {
+  const [currentWord, setCurrentWord] = useState("émotion");
+
+  const words = [
+    "histoire",
+    "passion",
+    "tradition",
+    "aventure",
+    "tentacion",
+    "responsabilité",
+    "innovation",
+    "engagement",
+    "expertise",
+    "nostalgie",
+  ];
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const intervalId = setInterval(() => {
+      currentIndex = (currentIndex + 1) % words.length;
+      setCurrentWord(words[currentIndex]);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <>
       <div className="w-full flex flex-col text-black overflow-hidden">
         <nav className="absolute top-0 left-0 w-full z-40">
           <div className="max-w-[1440px] mx-auto px-4 md:px-8 lg:px-12 w-full">
-            <div className="flex items-center justify-between py-2">
+            <div className="flex items-center justify-between py-6">
               <div className=" items-center">
-                <img src={logo} className="h-8" alt="logo" />
+                <img src={logo} className="h-10" alt="logo" />
               </div>
 
               <div className="flex gap-5 text-white text-xl">
@@ -38,6 +64,21 @@ function App() {
             muted
             loop
           ></video>
+        </div>
+        <div className="absolute top-[32rem] left-0 w-full z-40 text-white">
+          <div className="max-w-[1440px] mx-auto px-4 md:px-8 lg:px-12 w-full">
+            <p className="text-[4rem] font-medium leading-[50px] font-baskerville">
+              Mars n'est pas
+              <br /> qu'un chocolat, <br />
+              c'est une <span className="fade-in">{currentWord}</span>
+            </p>
+            <p className="text-slate-200 text-lg font-medium py-5 leading-6 max-w-[35rem]">
+              L’implantation du Groupe Mars en France date de 1967. Mars compte
+              aujourd’hui dans l’hexagone plus de 4 000 collaborateurs, 9 sites
+              dont 8 usines (4 en Alsace). Ces cinq dernières années, Mars a
+              investi en France plus de 300 millions d’euros.
+            </p>
+          </div>
         </div>
       </div>
     </>
